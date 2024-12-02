412e96123062c112b120d9c5bf629962669791d7
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-12-03 00:59:37 +0500

    Nextjs Project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <h2>Nav Item</h2>
         {children}
       </body>
     </html>

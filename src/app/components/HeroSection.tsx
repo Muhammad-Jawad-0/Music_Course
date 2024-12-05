67f2d03fe77a705bb28af751c5f99a29c7aae5ae
Muhammad Jawad <muhammadjawadforgithub@gmail.com>
2024-12-06 01:41:59 +0500

    complete hero Section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="skyblue"
+      />
       <div className="p-4 relative z-10 w-full text-center">
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master the art of music
@@ -13,7 +19,9 @@ const HeroSection = () => {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore Courses</Link>
+          <Link href={"/courses"}>
+            <Button>Explore Courses</Button>
+          </Link>
         </div>
       </div>
     </div>
